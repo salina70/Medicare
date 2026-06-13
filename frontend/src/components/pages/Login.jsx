61faee1efef0841d254848754c9a9a13686e4409
salina70 <salinamainali700@gmail.com>
2026-06-13 21:14:05 +0545

    added

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -25,8 +25,10 @@ function Login() {
     }
   }
 
-  const handleChange = (e) => {
+  const handleChange =  (e) => {
+
     const { name, value } = e.target;
+
 
     setFormData((prev) => ({
       ...prev,
