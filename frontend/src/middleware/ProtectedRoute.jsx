ab44cfa175063ab64e54c909b46cfe4a4930a78f
salina70 <salinamainali700@gmail.com>
2026-07-15 09:50:24 +0545

    chore: clean up unwated code

--- frontend/src/middleware/ProtectedRoute.jsx
+++ frontend/src/middleware/ProtectedRoute.jsx
@@ -1,25 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-// export function ProtectedRoute() {
-//   const storedLogin = localStorage.getItem("token");
-//   const loginData = storedLogin ? JSON.parse(storedLogin) : null;
-
-//   if (!loginData?.token)
-//     return <Navigate to="/login" />;
-//   return <Outlet />;
-// }
-
-// export function CheckAdmin() {
-//   const storedLogin = localStorage.getItem("token");
-//   console.log(storedLogin)
-//   const loginData = storedLogin ? storedLogin : null;
-// console.log(loginData)
-
-//   if (!loginData) return <Navigate to="/dashboard/admin" />;
-
-//   return <Outlet />;
-// }
-
 export function ProtectedRoute({ allowedRoles }) {
   const token = localStorage.getItem("token");
   const role = JSON.parse(localStorage.getItem("user")).role;
